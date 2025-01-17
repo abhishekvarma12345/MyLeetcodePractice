8b47a26ad0bd933e48f51e33dba49b65ba41663c
Dasaraju Abhishek Varma <53211164+abhishekvarma12345@users.noreply.github.com>
2025-01-17 11:06:37 +0100

    Time: 4 ms (71.76%), Space: 21.4 MB (40.05%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,27 +1,35 @@
 class MinStack:
 
     def __init__(self):
+        # Initialize main stack and minimum stack
         self.stack = []
         self.minStack = []
         
 
     def push(self, val: int) -> None:
+        # push to the main stack
         self.stack.append(val)
+        # update minimum stack if necessary
         if not self.minStack or val <= self.minStack[-1]:
             self.minStack.append(val)
 
     def pop(self) -> None:
+        # remove from main stack
         if self.stack:
+            # If element to be popped is the minimum
+            # remove from minimum stack
             if self.stack[-1] == self.minStack[-1]:
                 self.minStack.pop()
             self.stack.pop()
         
     def top(self) -> int:
+        # return top of the main stack
         if self.stack:
             return self.stack[-1]
         
 
     def getMin(self) -> int:
+        # return top of the minStack
         if self.minStack:
             return self.minStack[-1]
 
