3b3902dee30e93e38c4cd85936b40aafe0a14e42
Dasaraju Abhishek Varma <53211164+abhishekvarma12345@users.noreply.github.com>
2025-01-17 17:19:16 +0100

    Time: 542 ms (42.46%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,13 +1,8 @@
 # Write your MySQL query statement below
-with final as (
-    select count(*) as total
-    from Seat
-)
 select (case 
         when id%2=0 then id-1
-        when id=(select total from final) then id
+        when id=(select count(*) from Seat) then id
         else id+1 end) as id,
         student
 from Seat
-order by id
-;
+order by id;
